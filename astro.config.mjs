010288abb5767b34721433afb6f87d30127b0cc8
Frank Vera <frank.vera@coffeecode.com.mx>
2026-04-27 16:36:37 -0700

    Add custom domain Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
-  site: 'norson-supplies.coffeecode.com.mx',
+  site: 'https://norson-supplies.coffeecode.com.mx',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
